c2fa2f23d189f5060ca3037ef4ead09fb5468c20
Brian High <high@uw.edu>
2019-02-20 18:20:35 -0800

    Cleaned up nested loop and used more descriptive variable names.

--- mesim_like.R
+++ mesim_like.R
@@ -158,12 +158,12 @@ me_like <- function(n_subj = 10000, n_samp = 100, s3_sd1 = 1, s3_sd2 = 0.3) {
     }
     
     # Add additional parameters (a3hat, a3var, r2) for (1, 2) and (full, red)
-    ret.list$x[c('a3hat', 'a3var', 'r2')] <- NA
-    for (i in 1:2) {
-        for (j in c('full', 'red')) {
-            for (k in c('a3hat', 'a3var', 'r2')) {
-                varname <- paste0(k, '_', i, j)
-                ret.list[[paste0('xhat_', i, j)]][[k]] <- 
+    for (statvar in c('a3hat', 'a3var', 'r2')) {
+        ret.list$x[[statvar]] <- NA
+        for (spec in c('full', 'red')) {
+            for (samp in as.character(1:2)) {
+                varname <- paste0(statvar, '_', samp, spec)
+                ret.list[[paste0('xhat_', samp, spec)]][[statvar]] <- 
                     ifelse(exists(varname), get(varname), NA)
             }
         }
